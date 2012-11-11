2c1d920ce0e8b0692c39122e61105b828372c4a1
Damir Suleymanov <gbitle@gmail.com>
2012-11-11 17:08:15 -0500

    Added displaying question received from the server

--- Pasteur/ViewController.m
+++ Pasteur/ViewController.m
@@ -6,8 +6,10 @@
 //  Copyright (c) 2012 Harvard University. All rights reserved.
 //
 
+#import <ASIHTTPRequest/ASIHTTPRequest.h>
 #import <FacebookSDK/FacebookSDK.h>
 #import "ViewController.h"
+#import "SBJson.h"
 
 @implementation ViewController
 @synthesize textView;
@@ -46,7 +48,10 @@
                  NSLog(@"User info. [%@] - %@", user.id, user.name);
                  NSLog(@"Access Token: %@", FBSession.activeSession.accessToken);
 
-
+                 NSURL *url = [NSURL URLWithString:@"https://script.google.com/macros/s/AKfycbw0SfPRzLUVPHAnTVrXtdw5BThxGGiHyO7YaMmxmqpUycxjtto/exec"];
+                 ASIHTTPRequest *request = [ASIHTTPRequest requestWithURL:url];
+                 request.delegate = self;
+                 [request startAsynchronous];
                  //NSString *str = [NSString stringWithFormat:@"https://script.google.com/macros/s/AKfycbxwe6-wxW_gjoW05iKNiG_T0qez9uBWueYBQwWxf6g8qYQlSzse/exec?uid=%@&access_token=%@", user.id, FBSession.activeSession.accessToken];
                  //NSURL *url = [NSURL URLWithString:str];
                  //NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
@@ -99,6 +104,22 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)requestFinished:(ASIHTTPRequest *)request
+{
+    // Use when fetching text data
+    NSString *responseString = [request responseString];
+    NSLog(@"reponse: %@", responseString);
+
+    NSDictionary *question = [responseString JSONValue];
+    self.textView.text = [question objectForKey:@"question"];
+}
+
+- (void)requestFailed:(ASIHTTPRequest *)request
+{
+   NSError *error = [request error];
+    NSLog(@"response failed: %@", [error debugDescription]);
 }
 
 @end
